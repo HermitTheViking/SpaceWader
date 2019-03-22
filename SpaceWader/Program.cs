914331049cf5fed6982b60340ffb5b936a356245
HermitTheViking <30859033+HermitTheViking@users.noreply.github.com>
2019-03-22 19:45:33 +0100

    invader can move from top to bottom + borders at top / bottom

--- SpaceWader/Program.cs
+++ SpaceWader/Program.cs
@@ -8,8 +8,54 @@ namespace SpaceWader
 {
     class Program
     {
+        protected static int origRow;
+        protected static int origCol;
+
+        protected static void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected static void ClearCurrentConsoleLine(int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(' ');
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
         static void Main(string[] args)
         {
+            for (int i = 1; i < 11; i++)
+            {
+                string border = "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+";
+                WriteAt(border, 0, 0);
+                WriteAt(border, 0, 11);
+
+                string invader = "x";
+                WriteAt(invader, 0, i);
+
+                ClearCurrentConsoleLine(0, i-1);
+
+                System.Threading.Thread.Sleep(500);
+            }
+            
+            Console.ReadKey(true);
         }
     }
 }
